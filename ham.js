a1362912704510bac77bdf77e10c05835c61f318
vishnu jayakumar <vishnujayakumar1830@gmail.com>
2020-12-30 15:01:56 +0530

    second commit

--- ham.js
+++ ham.js
@@ -6,12 +6,32 @@ var close=document.querySelector('.close')
 ham.addEventListener('click',()=>{
 
     hey.classList.add('ham-click')
+    hey.style.animation=" move 1.5s ease-in-out"
+
 
     
 
 })
 
 close.addEventListener('click',()=>{
-    hey.classList.remove('ham-click')
+    
+    hey.style.animation=" remove 1.5s ease-in-out"
+   
+    setTimeout(()=>{
+        hey.classList.remove('ham-click')
+
+    },1400)
+    
+
+  
 
 })
+
+// function start(){
+
+//     hey.style.animation=" move 1s ease-in-out"
+
+
+
+
+// }
